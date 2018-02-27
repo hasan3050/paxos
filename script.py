67927a0eb6afd44906dcbe595894f9faf02c8351
fanlai0990 <31907044+fanlai0990@users.noreply.github.com>
2018-02-26 22:01:37 -0500

    Update script.py

--- script.py
+++ script.py
@@ -4,27 +4,27 @@ import commands
 NumofReplicas = 4
 NumofClients = 2
 
-def create_replicas(NumofReplicas = 6):
+def create_replicas(_NumofReplicas = 6):
 	_port = 9100
 	_id = 0
 	_p = 0
 	_timeout = 0.5 # second
 	_host = "127.0.0.1"
 
-	for server in range(NumofReplicas):
+	for server in range(_NumofReplicas):
 		cmd = "kill $(lsof -t -i:{0}) && python3 ./server.py --id {1} --host {2} --port {3} --p {4} --timeout {5} &".format(_p, _id, _host, _port, _p, _timeout)
 		print (os.system(cmd))
 		_id += 1
 		_port += 2
 
-def create_clients(NumofClients = 2):
+def create_clients(_NumofClients = 2):
 	_port = 9200
 	_id = 0
 	_p = 0
 	_timeout = 0.5 # second
 	_host = "127.0.0.1"
 
-	for server in range(NumofReplicas):
+	for server in range(_NumofClients):
 		cmd = "kill $(lsof -t -i:{0}) && python3 ./client.py --id {1} --host {2} --port {3} --p {4} --timeout {5} &".format(_p, _id, _host, _port, _p, _timeout)
 		print (os.system(cmd))
 		_id += 1
